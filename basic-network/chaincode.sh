a5e3418e9779ab5a01561ec074abebcb1d3625d4
unit09 <unit08@daum.net>
2020-10-12 13:13:39 +0900

    update
    채널을 하나만 사용하는 블록체인 네트워크로 변경 및 기획에 맞게 네트워크 구성 변경

--- basic-network/chaincode.sh
+++ basic-network/chaincode.sh
@@ -1,12 +1,10 @@
 set -ev
 
-docker exec cli1 peer chaincode install -n share-cc-1 -v 1.0 -p chaincode/go/channel1
-#docker exec cli2 peer chaincode install -n share-cc-2 -v 1.0 -p chaincode/go/channel2
+docker exec cli peer chaincode install -n share-cc-1 -v 1.0 -p chaincode/go/channel1
 sleep 1
 
-docker exec cli1 peer chaincode instantiate -o orderer.sharing.com:7050 -C channel1share -n share-cc-1 -v 1.0 -c '{"Args":[""]}' -P "OR ('AgencyOrg.member')"
-#docker exec cli2 peer chaincode instantiate -o orderer.sharing.com:7050 -C channel2share -n share-cc-2 -v 1.0 -c '{"Args":[""]}' -P "OR ('Share1Org.member','Share2Org.member')"
+docker exec cli peer chaincode instantiate -o orderer.sharing.com:7050 -C sharechannel1 -n share-cc-1 -v 1.0 -c '{"Args":[""]}' -P "OR ('AgencyOrg.member','Share1Org.member','Share2Org.member')"
 sleep 10
 
-docker exec cli1 peer chaincode query -o orderer.sharing.com:7050 -C channel1share -n share-cc-1 -c '{"function":"checkUser","Args":["test","test"]}'
+docker exec cli peer chaincode query -o orderer.sharing.com:7050 -C sharechannel1 -n share-cc-1 -c '{"function":"checkUser","Args":["test","test"]}'
 
